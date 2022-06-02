2a041cc8e526a4ed6c009d627868d61c92dafa88
Sean Purtill <seantpurtill@gmail.com>
2022-06-01 20:57:31 -0600

    Moving WIP Files to repository

---
+++ startbootstrap-grayscale-gh-pages/js/scripts.js
@@ -0,0 +1,96 @@
+/*!
+* Start Bootstrap - Grayscale v7.0.0 (https://startbootstrap.com/theme/grayscale)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    //
+    var randomizeMTN = function () {
+        const backGround = document.body.querySelector('#mastHead');
+
+        debugger
+        var rando = getRandomInt(5);
+
+
+        var image = 'assets/img/mtn' + rando + '.jpg';
+
+        backGround.style.backgroundImage = "url(" + image + ")";
+    };
+
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    var inputSend = function () {
+        debugger
+        var element = document.getElementById('summon');
+        var element = document.getElementById('theInputted');
+
+        var whereTheFuckWeGoing = 
+        window.location.href = "http://www.w3schools.com/";
+
+    };
+
+
+    navbarShrink();
+    randomizeMTN();
+    //inputSend();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
